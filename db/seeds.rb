51a78a82bab205b55f1da65fed310f99ce2c5e50
andreiabramovsky <andreiabramovsky@gmail.com>
2022-12-10 15:04:04 +0300

    make lesson4

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,42 @@
 
 answers = Answer.create!(
   [
-    {body: '', question_id: , correct: },
-    {body: '', question_id: , correct: },
-    {body: '', question_id: , correct: },
-    {body: '', question_id: , correct: },
-    {body: '', question_id: , correct: },
-    {body: '', question_id: , correct: },
-    {body: '', question_id: , correct: },
-    {body: '', question_id: , correct: },
-    {body: '', question_id: , correct: }
+    {body: 'Yes', question_id: questions.[0].id, correct: true},
+    {body: 'No', question_id: questions.[0].id, correct: false},
+    {body: 'Maybe yes', question_id: questions.[0].id, correct: false},
+    {body: 'No', question_id: questions.[1].id, correct: true},
+    {body: 'Yes', question_id: questions.[1].id, correct: false},
+    {body: 'Maybe no', question_id: questions.[1].id, correct: false},
+    {body: 'Draco Malfoy', question_id: questions.[2].id, correct: true},
+    {body: 'Neville Longbottom', question_id: questions.[2].id, correct: false},
+    {body: 'Luna Lovegood', question_id: questions.[2].id, correct: false},
+    {body: 'Diagon Alley', question_id: questions.[3].id, correct: true},
+    {body: 'The Burrow', question_id: questions.[3].id, correct: false},
+    {body: 'Knockturn Alley', question_id: questions.[3].id, correct: false},
+    {body: 'Arthur Weasley', question_id: questions.[4].id, correct: true},
+    {body: 'Molly Weasley', question_id: questions.[4].id, correct: false},
+    {body: 'William Weasley', question_id: questions.[4].id, correct: false},
+    {body: 'Pulp Fiction', question_id: questions.[5].id, correct: true},
+    {body: 'Reservoir Dogs', question_id: questions.[5].id, correct: false},
+    {body: 'Jackie Brown', question_id: questions.[5].id, correct: false},
+    {body: 'Popy', question_id: questions.[6].id, correct: true},
+    {body: 'Tulip', question_id: questions.[6].id, correct: false},
+    {body: 'Daisy', question_id: questions.[6].id, correct: false},
+    {body: 'German Spitz', question_id: questions.[7].id, correct: true},
+    {body: 'Mountain Cur', question_id: questions.[7].id, correct: false},
+    {body: 'Rhodesian Ridgeback', question_id: questions.[7].id, correct: false},
+    {body: 'Verb', question_id: questions.[8].id, correct: true},
+    {body: 'Noun', question_id: questions.[8].id, correct: false},
+    {body: 'Adjective', question_id: questions.[8].id, correct: false},
+    {body: 'Good', question_id: questions.[9].id, correct: true},
+    {body: 'Bad', question_id: questions.[9].id, correct: false},
+    {body: 'Not good', question_id: questions.[9].id, correct: false},
+    {body: 'Hawaii', question_id: questions.[10].id, correct: true},
+    {body: 'Madagascar', question_id: questions.[10].id, correct: false},
+    {body: 'Panama', question_id: questions.[10].id, correct: false},
+    {body: 'Eleven', question_id: questions.[11].id, correct: true},
+    {body: 'Twelve', question_id: questions.[11].id, correct: false},
+    {body: 'Thirteen', question_id: questions.[11].id, correct: false}
   ]
 )
 
@@ -37,8 +64,11 @@ questions = Question.create!(
     {body: 'Integer semper massa in eros faucibus?', test_id: tests[2].id},
     {body: 'Aliquam laoreet nisi non ante gravida rhoncus?', test_id: tests[2].id},
     {body: 'Praesent ornare nulla vitae lectus dapibus, ac accumsan libero blandit?', test_id: tests[3].id},
+    {body: 'Donec cursus, urna et tincidunt tempus, nibh orci tempus dui, et porttitor felis felis ut ex?', test_id: tests[3].id},
     {body: 'Ut posuere turpis dolor, sit amet vehicula ex facilisis nec?', test_id: tests[4].id},
-    {body: 'Vivamus volutpat lacus in metus lobortis vestibulum?', test_id: tests[5].id}
+    {body: 'Suspendisse id risus sit amet magna porta tincidunt sed in dui?', test_id: tests[4].id},
+    {body: 'Vivamus volutpat lacus in metus lobortis vestibulum?', test_id: tests[5].id},
+    {body: 'Praesent eget euismod massa, sit amet volutpat eros?', test_id: tests[5].id}
   ]
 )
 
@@ -58,5 +88,16 @@ users = User.create!(
     {name: 'Bob Marley'},
     {name: 'Lady GaGa'},
     {name: 'Steven Seagal'}
+  ]
+)
+
+user_tests = UserTest.create!(
+  [
+    { test_id: tests[0].id, user_id: users[0].id },
+    { test_id: tests[1].id, user_id: users[1].id },
+    { test_id: tests[2].id, user_id: users[1].id },
+    { test_id: tests[3].id, user_id: users[2].id },
+    { test_id: tests[0].id, user_id: users[2].id },
+    { test_id: tests[4].id, user_id: users[0].id }
   ]
 )
