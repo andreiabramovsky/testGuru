4c1189d242610da41c16555783ae15b9f67a4715
andreiabramovsky <andreiabramovsky@gmail.com>
2022-12-12 19:40:57 +0300

    correction seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,52 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Answer.create!(
-  [
-    {body: 'Yes', question_id: questions.[0].id, correct: true},
-    {body: 'No', question_id: questions.[0].id, correct: false},
-    {body: 'Maybe yes', question_id: questions.[0].id, correct: false},
-    {body: 'No', question_id: questions.[1].id, correct: true},
-    {body: 'Yes', question_id: questions.[1].id, correct: false},
-    {body: 'Maybe no', question_id: questions.[1].id, correct: false},
-    {body: 'Draco Malfoy', question_id: questions.[2].id, correct: true},
-    {body: 'Neville Longbottom', question_id: questions.[2].id, correct: false},
-    {body: 'Luna Lovegood', question_id: questions.[2].id, correct: false},
-    {body: 'Diagon Alley', question_id: questions.[3].id, correct: true},
-    {body: 'The Burrow', question_id: questions.[3].id, correct: false},
-    {body: 'Knockturn Alley', question_id: questions.[3].id, correct: false},
-    {body: 'Arthur Weasley', question_id: questions.[4].id, correct: true},
-    {body: 'Molly Weasley', question_id: questions.[4].id, correct: false},
-    {body: 'William Weasley', question_id: questions.[4].id, correct: false},
-    {body: 'Pulp Fiction', question_id: questions.[5].id, correct: true},
-    {body: 'Reservoir Dogs', question_id: questions.[5].id, correct: false},
-    {body: 'Jackie Brown', question_id: questions.[5].id, correct: false},
-    {body: 'Popy', question_id: questions.[6].id, correct: true},
-    {body: 'Tulip', question_id: questions.[6].id, correct: false},
-    {body: 'Daisy', question_id: questions.[6].id, correct: false},
-    {body: 'German Spitz', question_id: questions.[7].id, correct: true},
-    {body: 'Mountain Cur', question_id: questions.[7].id, correct: false},
-    {body: 'Rhodesian Ridgeback', question_id: questions.[7].id, correct: false},
-    {body: 'Verb', question_id: questions.[8].id, correct: true},
-    {body: 'Noun', question_id: questions.[8].id, correct: false},
-    {body: 'Adjective', question_id: questions.[8].id, correct: false},
-    {body: 'Good', question_id: questions.[9].id, correct: true},
-    {body: 'Bad', question_id: questions.[9].id, correct: false},
-    {body: 'Not good', question_id: questions.[9].id, correct: false},
-    {body: 'Hawaii', question_id: questions.[10].id, correct: true},
-    {body: 'Madagascar', question_id: questions.[10].id, correct: false},
-    {body: 'Panama', question_id: questions.[10].id, correct: false},
-    {body: 'Eleven', question_id: questions.[11].id, correct: true},
-    {body: 'Twelve', question_id: questions.[11].id, correct: false},
-    {body: 'Thirteen', question_id: questions.[11].id, correct: false}
-  ]
-)
-
 categories = Category.create!(
   [
     {title: 'Animals'},
     {title: 'Plants'},
     {title: 'Sports'}
+  ]
+)
+
+users = User.create!(
+  [
+    {name: 'Bob Marley'},
+    {name: 'Lady GaGa'},
+    {name: 'Steven Seagal'}
+  ]
+)
+
+tests = Test.create!(
+  [
+    {title: 'Cats', level: 0, author_id: users[0].id, category_id: categories[0].id},
+    {title: 'Dogs', level: 1, author_id: users[1].id, category_id: categories[0].id},
+    {title: 'Flowers', level: 1, author_id: users[0].id, category_id: categories[1].id},
+    {title: 'Woods', level: 2, author_id: users[2].id, category_id: categories[1].id},
+    {title: 'Chess', level: 2, author_id: users[2].id, category_id: categories[2].id},
+    {title: 'Checkers', level: 3, author_id: users[2].id, category_id: categories[2].id}
   ]
 )
 
@@ -72,26 +50,48 @@ questions = Question.create!(
   ]
 )
 
-tests = Test.create!(
+answers = Answer.create!(
   [
-    {title: 'Cats', level: 0, author_id: users[0].id, category_id: categories[0].id},
-    {title: 'Dogs', level: 1, author_id: users[1].id, category_id: categories[0].id},
-    {title: 'Flowers', level: 1, author_id: users[0].id, category_id: categories[1].id},
-    {title: 'Woods', level: 2, author_id: users[2].id, category_id: categories[1].id},
-    {title: 'Chess', level: 2, author_id: users[2].id, category_id: categories[2].id},
-    {title: 'Checkers', level: 3, author_id: users[2].id, category_id: categories[2].id}
+    {body: 'Yes', question_id: questions[0].id, correct: true},
+    {body: 'No', question_id: questions[0].id, correct: false},
+    {body: 'Maybe yes', question_id: questions[0].id, correct: false},
+    {body: 'No', question_id: questions[1].id, correct: true},
+    {body: 'Yes', question_id: questions[1].id, correct: false},
+    {body: 'Maybe no', question_id: questions[1].id, correct: false},
+    {body: 'Draco Malfoy', question_id: questions[2].id, correct: true},
+    {body: 'Neville Longbottom', question_id: questions[2].id, correct: false},
+    {body: 'Luna Lovegood', question_id: questions[2].id, correct: false},
+    {body: 'Diagon Alley', question_id: questions[3].id, correct: true},
+    {body: 'The Burrow', question_id: questions[3].id, correct: false},
+    {body: 'Knockturn Alley', question_id: questions[3].id, correct: false},
+    {body: 'Arthur Weasley', question_id: questions[4].id, correct: true},
+    {body: 'Molly Weasley', question_id: questions[4].id, correct: false},
+    {body: 'William Weasley', question_id: questions[4].id, correct: false},
+    {body: 'Pulp Fiction', question_id: questions[5].id, correct: true},
+    {body: 'Reservoir Dogs', question_id: questions[5].id, correct: false},
+    {body: 'Jackie Brown', question_id: questions[5].id, correct: false},
+    {body: 'Popy', question_id: questions[6].id, correct: true},
+    {body: 'Tulip', question_id: questions[6].id, correct: false},
+    {body: 'Daisy', question_id: questions[6].id, correct: false},
+    {body: 'German Spitz', question_id: questions[7].id, correct: true},
+    {body: 'Mountain Cur', question_id: questions[7].id, correct: false},
+    {body: 'Rhodesian Ridgeback', question_id: questions[7].id, correct: false},
+    {body: 'Verb', question_id: questions[8].id, correct: true},
+    {body: 'Noun', question_id: questions[8].id, correct: false},
+    {body: 'Adjective', question_id: questions[8].id, correct: false},
+    {body: 'Good', question_id: questions[9].id, correct: true},
+    {body: 'Bad', question_id: questions[9].id, correct: false},
+    {body: 'Not good', question_id: questions[9].id, correct: false},
+    {body: 'Hawaii', question_id: questions[10].id, correct: true},
+    {body: 'Madagascar', question_id: questions[10].id, correct: false},
+    {body: 'Panama', question_id: questions[10].id, correct: false},
+    {body: 'Eleven', question_id: questions[11].id, correct: true},
+    {body: 'Twelve', question_id: questions[11].id, correct: false},
+    {body: 'Thirteen', question_id: questions[11].id, correct: false}
   ]
 )
 
-users = User.create!(
-  [
-    {name: 'Bob Marley'},
-    {name: 'Lady GaGa'},
-    {name: 'Steven Seagal'}
-  ]
-)
-
-UserTest.create!(
+userTests = UserTest.create!(
   [
     { test_id: tests[0].id, user_id: users[0].id },
     { test_id: tests[1].id, user_id: users[1].id },
