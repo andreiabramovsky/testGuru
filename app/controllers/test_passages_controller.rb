f5d2a040e7479ab8eb9731e6f1a13ac47bc03409
andreiabramovsky <andreiabramovsky@gmail.com>
2023-02-03 21:35:08 +0300

    refactoring test_passage

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,15 +1,19 @@
 class TestPassagesController < ApplicationController
 
-  def show
-    
-  end
+  before_action :set_test_passage, only: %i[ result show update ]
 
-  def result
-    
-  end
+  def show; end
+
+  def result; end
 
   def update
-    
+    @test_passage.accept!(params[:answer_ids])
+
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
   end
 
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,7 @@ class Test < ApplicationRecord
 
   has_many :questions, dependent: :destroy
   has_many :test_passages, dependent: :destroy
-  has_many :users, through: :test_passagesdb
+  has_many :users, through: :test_passages
 
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
