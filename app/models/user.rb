507bb988ee151d76d72d43d26cbc8f40ae8b8b0c
andreiabramovsky <andreiabramovsky@gmail.com>
2023-02-12 18:56:24 +0300

    add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
+  
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
@@ -9,8 +15,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, 
             format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  has_secure_password
 
   def test_passage(test)
     test_passage.order(id: :desc).find_by(test_id: test.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
   root 'tests#index'
+
+  devise_for :users, path: :gurus,
+                     path_names: { sign_in: :login, sign_out: :logout, sign_up: :signup }
   
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  delete :logout, to: 'sessions#destroy'
-
-  resources :users, only: :create
-  resources :sessions, only: :create
-
   resources :tests do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
