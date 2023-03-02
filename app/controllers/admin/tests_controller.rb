15b62d710d2af94932e08fa3437d46db903ddae5
andreiabramovsky <andreyabramovsky@gmail.com>
2023-03-02 21:31:26 +0300

    add localization

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -20,10 +20,23 @@ class Admin::TestsController < Admin::BaseController
     @test = current_user.authored_tests.new(test_params)
 
     if @test.save
-      redirect_to admin_test_path(@test), notice: 'Test was successfully created.'
+      redirect_to admin_test_path(@test), notice: t('.success')
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to admin_test_path(@test), notice: t('.update')
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path, notice: t('.delete')
   end
 
   private
