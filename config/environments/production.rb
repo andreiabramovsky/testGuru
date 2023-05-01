6462aec46801bae49790f5e5f07723c42c41b7e2
andreiabramovsky <andreyabramovsky@gmail.com>
2023-05-01 18:40:00 +0300

    setting action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "testGuru_production"
-
+  config.action_mailer.default_url_options = { host: 'test-guru-3oom.onrender.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
