d60750dc07338099194072584106aed651c0825f
andreiabramovsky <andreyabramovsky@gmail.com>
2023-05-05 22:41:46 +0300

    setting smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     tls: true,
     address: 'smtp.yandex.com',
-    port: 587,
+    port: 465,
     domain: 'yandex.ru',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
