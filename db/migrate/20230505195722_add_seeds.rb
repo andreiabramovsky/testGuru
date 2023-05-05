2090669afd9ad51db5e2fce277f62218205c7d62
andreiabramovsky <andreyabramovsky@gmail.com>
2023-05-06 00:16:30 +0300

    add admin

--- db/migrate/20230505195722_add_seeds.rb
+++ db/migrate/20230505195722_add_seeds.rb
@@ -1,6 +1,5 @@
 class AddSeeds < ActiveRecord::Migration[6.1]
   def up
-    Admin.create_with(name: 'Andrei', first_name: 'Andrei', last_name: 'Andrei', password: '123456').find_or_create_by(email: "andreabramovsky+admin@gmail.com")
     tests = [
       {
           author: 'andreiabramovsky@gmail.com',
---
+++ db/migrate/20230505211444_create_admin.rb
@@ -0,0 +1,5 @@
+class CreateAdmin < ActiveRecord::Migration[6.1]
+  def up
+    Admin.create_with(name: 'Andrei', first_name: 'Andrei', last_name: 'Andrei', password: '123456', password_confirmation: '123456').find_or_create_by(email: "andreabramovsky+admin@gmail.com")
+  end
+end
